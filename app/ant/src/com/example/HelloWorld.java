3dc83906a1de1ea9fac818717ac8e06f2b760637
yeonho.ryu <kell0@kuberix.com>
2024-06-18 05:27:47 +0000

    Initial commit with HelloWorld Java code and build script

---
+++ app/ant/src/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+// src/com/example/HelloWorld.java
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
